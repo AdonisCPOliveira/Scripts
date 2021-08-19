cff23e7cd9894526c09fd5dfc30a84569639bc03
grillorj <leonardo.grillo@gmail.com>
2021-08-18 23:27:28 -0300

    Add files via upload

--- # SCRIPT PARA USUARIOS EM GRUPOS AD.ps1
+++ # SCRIPT PARA USUARIOS EM GRUPOS AD.ps1
@@ -2,10 +2,19 @@ Connect-AzureAD
 
 #ALTERE AQUI AS OPCOES
 $disc = "E:\" #ONDE VAI GRAVAR O CSV
-$GroupName = "VPN-servers-svn" #NOME DO GRUPO
+
 
 #EXECUÇÃO DO COMANDO - NAO ALTERAR
-# Get-AzureADGroup -SearchString "xxx" #PROCURA TODOS GRUPOS COM NOME
-$Group = Get-AzureADGroup -Filter "DisplayName eq '$GroupName'"
-Get-AzureADGroupMember -ObjectId $Group.ObjectId -All $true  |  Select DisplayName, UserPrincipalName, department, mail, physicalDeliveryOfficeName, title |
-Export-CSV $disc"GroupMembers_$GroupName.csv" -NoTypeInformation -Encoding UTF8
+
+$list_group = Get-AzureADGroup -SearchString "DHCP Administrators" #PROCURA TODOS GRUPOS COM NOME
+#$list_group = Get-AzureADGroup -Filter "DisplayName eq '$GroupName'" #PROCURA POR UM GRUPO
+
+if ($list_group -ne $null) {
+    for ($i=0; $i -lt $list_group.Length;$i++){
+        $groupobj = $list_group[$i].ObjectId
+        $GroupName = $list_group[$i].DisplayName
+        $b = $i
+        $get_group_info = Get-AzureADGroupMember -ObjectId $groupobj -All $true  |  Select DisplayName, UserPrincipalName, department, mail, physicalDeliveryOfficeName, title |
+        export-CSV $disc"GroupMembers_$GroupName.csv" -NoTypeInformation -Encoding UTF8
+    }}
+    
