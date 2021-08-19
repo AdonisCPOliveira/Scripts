c20fa77753882db2671e3ee364dc6cfdeccdb9cc
grillorj <leonardo.grillo@gmail.com>
2021-08-18 23:39:23 -0300

    Add files via upload
    Removido variáveis inúteis!

--- # SCRIPT PARA USUARIOS EM GRUPOS AD.ps1
+++ # SCRIPT PARA USUARIOS EM GRUPOS AD.ps1
@@ -6,15 +6,14 @@ $disc = "E:\" #ONDE VAI GRAVAR O CSV
 
 #EXECUÇÃO DO COMANDO - NAO ALTERAR
 
-$list_group = Get-AzureADGroup -SearchString "DHCP Administrators" #PROCURA TODOS GRUPOS COM NOME
+$list_group = Get-AzureADGroup -SearchString "Administrators" #PROCURA TODOS GRUPOS COM NOME
 #$list_group = Get-AzureADGroup -Filter "DisplayName eq '$GroupName'" #PROCURA POR UM GRUPO
 
 if ($list_group -ne $null) {
     for ($i=0; $i -lt $list_group.Length;$i++){
         $groupobj = $list_group[$i].ObjectId
         $GroupName = $list_group[$i].DisplayName
-        $b = $i
-        $get_group_info = Get-AzureADGroupMember -ObjectId $groupobj -All $true  |  Select DisplayName, UserPrincipalName, department, mail, physicalDeliveryOfficeName, title |
+        Get-AzureADGroupMember -ObjectId $groupobj -All $true  |  Select DisplayName, UserPrincipalName, department, mail, physicalDeliveryOfficeName, title |
         export-CSV $disc"GroupMembers_$GroupName.csv" -NoTypeInformation -Encoding UTF8
     }}
     
