fb7078bfff2dc4e66f63fd65fe99417f572d3cf0
grillorj <leonardo.grillo@gmail.com>
2021-10-29 12:45:15 -0300

    Create Check Users not in groups.ps1

---
+++ Check Users not in groups.ps1
@@ -0,0 +1,9 @@
+Connect-AzureAD
+$groupids = @("222f0282-faa6-466b-8721-b546485ea6eb", "4a45259f-c5e0-4c06-bc83-3590e3a74fa1", "c5d2a7b1-b239-4f1e-b36f-006da99ee48b", "69fe9f48-4c30-4b9b-99f9-1270de3ea85f", "25886613-fe32-443c-9a61-68685923f266", "bdfc32ca-b272-4e55-9edf-52ec5ce36d01", "7156a7c5-77c6-4ef4-929f-f479fcd885fe", "8fe4df62-a41a-4229-88dc-16b6e30cd0fb", "1202b774-fad1-4f8c-a54a-35a5e9b9eddf", "eea7a33a-8c24-42a8-852c-98f15f3d037a")
+
+$userht = @{}
+Get-AzureADUser -all $true | foreach-object {$userht.Add($_.ObjectId,$_)} 
+ForEach($id  in  $groupids){
+Get-AzureADUserMembership -all $true -ObjectId $id | foreach-object { $userht.Remove($_.ObjectId) }
+}
+$userht.Values | Select-Object DisplayName, UserPrincipalName, mail | export-CSV "c:\temp\GroupMembers.csv" -NoTypeInformation -Encoding UTF8
