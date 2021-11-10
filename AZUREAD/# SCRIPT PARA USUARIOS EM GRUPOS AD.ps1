ed66d3725989e542b783d7f15f9908965fa56153
grillorj <leonardo.grillo@gmail.com>
2021-11-10 16:31:21 -0300

    update
    up

---
+++ AZUREAD/# SCRIPT PARA USUARIOS EM GRUPOS AD.ps1
@@ -0,0 +1,133 @@
+#### COMMENT #####
+### Comment 2 ###
+
+Connect-AzureAD
+
+# Imput dados Unidade
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Entre com os Dados'
+$form.Size = New-Object System.Drawing.Size(300,200)
+$form.StartPosition = 'CenterScreen'
+
+$okButton = New-Object System.Windows.Forms.Button
+$okButton.Location = New-Object System.Drawing.Point(75,120)
+$okButton.Size = New-Object System.Drawing.Size(75,23)
+$okButton.Text = 'OK'
+$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $okButton
+$form.Controls.Add($okButton)
+
+$cancelButton = New-Object System.Windows.Forms.Button
+$cancelButton.Location = New-Object System.Drawing.Point(150,120)
+$cancelButton.Size = New-Object System.Drawing.Size(75,23)
+$cancelButton.Text = 'Cancel'
+$cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $cancelButton
+$form.Controls.Add($cancelButton)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10,20)
+$label.Size = New-Object System.Drawing.Size(280,20)
+$label.Text = 'Descreva a unidade a ser Gravada. EX d:\'
+$form.Controls.Add($label)
+
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Location = New-Object System.Drawing.Point(10,40)
+$textBox.Size = New-Object System.Drawing.Size(260,20)
+$form.Controls.Add($textBox)
+
+$form.Topmost = $true
+
+$form.Add_Shown({$textBox.Select()})
+$result = $form.ShowDialog()
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $disc = $textBox.Text
+    
+}
+
+
+#Cria Pastas
+Function GenerateFolder($path) {
+    $global:foldPath = $null
+    foreach($foldername in $path.split("\")) {
+        $global:foldPath += ($foldername+"\")
+        if (!(Test-Path $global:foldPath)){
+            New-Item -ItemType Directory -Path $global:foldPath
+            
+        }
+    }
+}
+
+
+#imput Dados Grupos
+
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Entre com os Dados'
+$form.Size = New-Object System.Drawing.Size(300,200)
+$form.StartPosition = 'CenterScreen'
+
+$okButton = New-Object System.Windows.Forms.Button
+$okButton.Location = New-Object System.Drawing.Point(75,120)
+$okButton.Size = New-Object System.Drawing.Size(75,23)
+$okButton.Text = 'OK'
+$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $okButton
+$form.Controls.Add($okButton)
+
+$cancelButton = New-Object System.Windows.Forms.Button
+$cancelButton.Location = New-Object System.Drawing.Point(150,120)
+$cancelButton.Size = New-Object System.Drawing.Size(75,23)
+$cancelButton.Text = 'Cancel'
+$cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $cancelButton
+$form.Controls.Add($cancelButton)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10,20)
+$label.Size = New-Object System.Drawing.Size(280,20)
+$label.Text = 'Descreva o grupo ou parte dele para pesquisa'
+$form.Controls.Add($label)
+
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Location = New-Object System.Drawing.Point(10,40)
+$textBox.Size = New-Object System.Drawing.Size(260,20)
+$form.Controls.Add($textBox)
+
+$form.Topmost = $true
+
+$form.Add_Shown({$textBox.Select()})
+$result = $form.ShowDialog()
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $groupimput = $textBox.Text
+    
+}
+
+#$disc
+#$groupimput
+
+#ALTERE AQUI AS OPCOES
+#$disc = "E:\" #ONDE VAI GRAVAR O CSV
+
+
+#EXECUÇÃO DO COMANDO - NAO ALTERAR
+GenerateFolder $disc
+$list_group = Get-AzureADGroup -SearchString "$groupimput" #PROCURA TODOS GRUPOS COM NOME
+#$list_group = Get-AzureADGroup -Filter "DisplayName eq '$GroupName'" #PROCURA POR UM GRUPO
+
+if ($null -ne $list_group) {
+    for ($i=0; $i -lt $list_group.Length;$i++){
+        $groupobj = $list_group[$i].ObjectId
+        $GroupName = $list_group[$i].DisplayName
+        Get-AzureADGroupMember -ObjectId $groupobj -All $true  |  Select-Object DisplayName, UserPrincipalName, department, mail, physicalDeliveryOfficeName, title |
+        export-CSV $disc"\GroupMembers_$GroupName.csv" -NoTypeInformation -Encoding UTF8
+    }}
