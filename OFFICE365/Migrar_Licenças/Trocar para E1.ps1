32eebbb8705d35ddf1d8ab779329611bf245de50
Leonardo Grillo Duarte <leonardo.grillo@gmail.com>
2021-11-29 17:40:58 -0300

    Add files via upload

---
+++ OFFICE365/Migrar_Licenças/Trocar para E1.ps1
@@ -0,0 +1,42 @@
+﻿##
+## 
+## Script para alteração de licença em massa
+##	remove E3 e F3
+##	adiciona E1
+##
+
+
+$lista = Import-Csv "C:\temp\E1.CSV" #Gerar CSV a partir do XLS, colocar na primeira linha UserPrincipalName como título
+
+foreach($u in $lista){
+
+    $user = $u.UserPrincipalName
+
+    #-------Definições das licenças no ambiente Hitss-------
+    #      
+    #      F3 = "reseller-account:DESKLESSPACK"
+    #      E1 = "reseller-account:STANDARDPACK"
+    #      E3 = "reseller-account:ENTERPRISEPACK"
+    #
+       
+
+    if ((Get-MsolUser -UserPrincipalName $user | Select -ExpandProperty Licenses | Select-Object accountskuid) -like "*ENTERPRISEPACK*" ){
+
+        Set-MsolUserLicense -UserPrincipalName $user -RemoveLicenses "reseller-account:ENTERPRISEPACK"
+        echo "licença E3 Removida"
+    }
+    if ((Get-MsolUser -UserPrincipalName $user | Select -ExpandProperty Licenses | Select-Object accountskuid) -like "*DESKLESSPACK*" ){
+        
+        Set-MsolUserLicense -UserPrincipalName $user -RemoveLicenses "reseller-account:DESKLESSPACK"
+        echo "licença F3 Removida"
+    }   
+          
+    Set-MsolUserLicense -UserPrincipalName $user -AddLicenses "reseller-account:STANDARDPACK"
+
+        echo "Licença E1 atribuída para $user"
+    
+    
+
+    
+
+}
---
+++ OFFICE365/Migrar_Licenças/Trocar para E3.ps1
@@ -0,0 +1,44 @@
+﻿##
+## 
+## Script para alteração de licença em massa
+##	remove E1 e F3
+##	adiciona E3
+##
+
+
+
+
+$lista = Import-Csv "C:\temp\E3.CSV"
+
+foreach($u in $lista){
+
+    $user = $u.UserPrincipalName
+
+    #-------Definições das licenças no ambiente Hitss-------
+    #      
+    #      F3 = "reseller-account:DESKLESSPACK"
+    #      E1 = "reseller-account:STANDARDPACK"
+    #      E3 = "reseller-account:ENTERPRISEPACK"
+    #
+
+
+    if ((Get-MsolUser -UserPrincipalName $user | Select -ExpandProperty Licenses | Select-Object accountskuid) -like "*STANDARDPACK*" ){
+
+        Set-MsolUserLicense -UserPrincipalName $user -RemoveLicenses "reseller-account:STANDARDPACK"
+        echo "licença E1 Removida"
+    }
+    if ((Get-MsolUser -UserPrincipalName $user | Select -ExpandProperty Licenses | Select-Object accountskuid) -like "*DESKLESSPACK*" ){
+        
+        Set-MsolUserLicense -UserPrincipalName $user -RemoveLicenses "reseller-account:DESKLESSPACK"
+        echo "licença F3 Removida"
+    }   
+          
+    Set-MsolUserLicense -UserPrincipalName $user -AddLicenses "reseller-account:ENTERPRISEPACK"
+
+        echo "Licença E3 atribuída para $user"
+    
+    
+
+    
+
+}
