75c2abacc992c11d2c39328b680db70b966bbb73
Leonardo Grillo Duarte <leonardo.grillo@gmail.com>
2021-12-08 17:02:59 -0300

    Add files via upload

---
+++ OFFICE365/lixao0365.ps1
@@ -0,0 +1,23 @@
+﻿##
+## 
+## Script para alteração de licença em massa
+##	remove E3 e F3 E1 TEAMS e Desabilita as contas
+##	
+##
+
+
+$lista = Import-Csv "C:\temp\REMOVE.CSV" #Gerar CSV a partir do XLS, colocar na primeira linha UserPrincipalName como título
+
+foreach($u in $lista){
+
+    $user = $u.UserPrincipalName
+
+    #REMOVE COM LIXEIRA  
+    #Remove-AzureADUser -ObjectId $user
+   
+    #REMOVE SEM LIXEIRA
+    Remove-MsolUser -UserPrincipalName  $user
+ echo "$user Excluído"
+    
+
+}
