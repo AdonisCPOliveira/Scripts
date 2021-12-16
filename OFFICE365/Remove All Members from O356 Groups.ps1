edfd2bd95bcdd8d56e7b3e2347d5a60904a73fee
Leonardo Grillo Duarte <leonardo.grillo@gmail.com>
2021-12-16 12:00:36 -0300

    Add files via upload

---
+++ OFFICE365/Remove All Members from O356 Groups.ps1
@@ -0,0 +1,15 @@
+﻿#connect to Exchange Online
+$Cred = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $Cred -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+
+
+$DistributionGroup = "ajustescontaso365" #O365 Group
+
+#Get List of Group Members
+$list = Get-DistributionGroupMember -Identity $DistributionGroup -ResultSize 1500 #ResultSize -> Result Max Return
+
+#Remove all Users
+$list | % {
+   Remove-DistributionGroupMember -Identity $DistributionGroup -Member $_.Name -Confirm:$false
+   } 
