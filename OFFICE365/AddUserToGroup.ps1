e2d65c9941d36fe173426105767a5e3747b95b46
Leonardo Grillo Duarte <leonardo.grillo@gmail.com>
2021-12-16 11:58:16 -0300

    Add files via upload

--- OFFICE365/AddUserToGroup.ps1
+++ OFFICE365/AddUserToGroup.ps1
@@ -1,30 +1,15 @@
-
-
-
-
-###############################################################################################
-
-###   CASO SEJA CRIADO UM NOVO GRUPO OU OS GRUPOS ACIMA APRESENTEM ERRO, FAVOR ACIONAR O    ###
-###   TIME DE OFFICE 365.                                                                   ###
-###   USAR PSESSION DO EXCHANGE ONLINE
-###############################################################################################
-
-
+﻿
 #--------- Importando o arquivo com os usuários e seus respectivos grupos
 
-$users = Import-Csv ".\teste.csv" -Delimiter ';' #-> Alterar o delimiter para ; ou , de acordo com o resultado do salvamento no excel
+$users = Import-Csv "c:\temp\TempUser.csv" #-Delimiter ';' #-> Alterar o delimiter para ; ou , de acordo com o resultado do salvamento no excel
 
 #--------- Bloco de varredura para atribuir cada usuário para o grupo desejado.
 
+$groupname = "ajustescontaso365"
+
 foreach($u in $users){
 
-#$userobjectid = (Get-AzureADUser -objectid $u.upn).objectid
-#$gid = $u.GroupID
+    Add-DistributionGroupMember -identity "$groupname" -Member $u.upn
 
-#Add-AzureADGroupMember -ObjectId $gid -RefObjectId $userobjectid     ##Grupos O365 (Passamos uma raiva até descobrir)
-
-Add-DistributionGroupMember -identity "Temp_list_comunica" -Member $u.upn
-
-
-}
-
+    Echo "$u Adicionado ao Grupo $Groupname"
+    }
